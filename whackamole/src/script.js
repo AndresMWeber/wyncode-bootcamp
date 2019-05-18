d294da79a56df996eb1cfaafbfb27cb9a535558f
andresweber <andresmweber@gmail.com>
2019-05-18 11:14:50 -0400

    setting the stage.  Added basic JS functions.

--- whackamole/src/script.js
+++ whackamole/src/script.js
@@ -1,5 +1,21 @@
 
 
 function startGame() {
-    console.log('this is start game')
+
+}
+
+function modify_score(value) {
+    var score = document.getElementById('score')
+    if (parseInt(score.innerHTML) === 10) {
+        window.alert('You have won the game!  Congrats!')
+        score.innerHTML = 0
+    }
+    else {
+        score.innerHTML = parseInt(score.innerHTML) + value
+    }
+}
+
+function bonk(bowl) {
+    console.log(bowl)
+    modify_score(1)
 }
