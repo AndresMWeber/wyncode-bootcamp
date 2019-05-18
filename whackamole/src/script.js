fc6a2d67b0cbc4626c50d9faf3164f9682a2ee09
andresweber <andresmweber@gmail.com>
2019-05-18 12:30:10 -0400

    Added all game logic and CSS styling.

--- whackamole/src/script.js
+++ whackamole/src/script.js
@@ -1,21 +1,30 @@
-
+var game_state = 1;
 
 function startGame() {
+    setInterval(() => {
+        update();
+    }, 1000);
+}
 
+function update() {
+    var avocados = document.getElementsByClassName('avocado');
+    var random_avocado = avocados[parseInt(Math.random() * 6)];
+    random_avocado.classList.add('up');
+    setTimeout(() => random_avocado.classList.remove('up'), 1000);
 }
 
 function modify_score(value) {
-    var score = document.getElementById('score')
+    var score = document.getElementById('score');
     if (parseInt(score.innerHTML) === 10) {
-        window.alert('You have won the game!  Congrats!')
-        score.innerHTML = 0
+        window.alert('You have won the game!  Congrats!');
+        score.innerHTML = 0;
+        game_state = 0;
     }
     else {
-        score.innerHTML = parseInt(score.innerHTML) + value
+        score.innerHTML = parseInt(score.innerHTML) + value;
     }
 }
 
 function bonk(bowl) {
-    console.log(bowl)
-    modify_score(1)
+    modify_score(1);
 }
